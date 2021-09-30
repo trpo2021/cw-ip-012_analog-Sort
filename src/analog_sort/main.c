5bf8e6236f695d2b416137b88bf759ccb397d430
Ivan Perminov <i.s.perminov@yandex.ru>
2021-09-30 11:42:39 +0700

    process file

--- src/analog_sort/main.c
+++ src/analog_sort/main.c
@@ -1,12 +1,31 @@
 #include <lib_sort/arg.h>
 #include <lib_sort/startprog.h>
+#include <lib_sort/processfile.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv){
     
     start();
 	Params params = {NULL, NULL};
 	opc(&params, argc, argv);
+
+	FILE* fin;
+       if (params.input != NULL) {
+               fin = fopen(params.input, "r");
+       } else {
+               exit(EXIT_FAILURE);
+       }
+       char** lines = get_lines_from_file(fin);
+
+       printf("line0: %s\n", lines[0]);
+       printf("line1: %s\n", lines[1]);
+       printf("line2: %s\n", lines[2]);
+       printf("line3: %s\n", lines[3]);
+       printf("line4: %s\n", lines[4]);
+       printf("line5: %s\n", lines[5]);
+
+
 	return 0;
 }
---
+++ src/lib_sort/processfile.h
@@ -0,0 +1,10 @@
+#ifndef PROCESSFILE_H
+#define PROCESSFILE_H
+
+#include <stdio.h>
+
+char* get_line(FILE*);
+
+char** get_lines_from_file(FILE*);
+
+#endif // #ifndef PROCESSFILE_H
