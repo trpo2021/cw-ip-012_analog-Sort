96a3b72ee93aec1777e549ab720d06ac64f48a1e
Ivan Perminov <i.s.perminov@yandex.ru>
2021-10-06 11:06:24 +0700

    Write processfile_test.c

--- test/processfile_test.c
+++ test/processfile_test.c
@@ -6,7 +6,26 @@
 
 CTEST(suite_processfile, get_line)
 {
+    FILE* fin = fopen("test/file_test.txt", "r");
+    if (fin == NULL){
+        fprintf(stderr, "Error! Can't open test/file_test.txt");
+    }
+    
+    char* line = get_line(fin);
+    check_str("   ", line);
 
+    char* line2 = get_line(fin);
+    check_str("", line2);
+
+    char* line3 = get_line(fin);
+    check_str("Math file", line3);
+    
+    fclose(fin);
+    fclose(line);
+    fclose(line2);
+    fclose(line3);
+    
+    
 }
 CTEST(suite_processfile, get_lines_from_file)
 {
