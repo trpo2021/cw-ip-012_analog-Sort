948f1a7ae140a5b1424ce6b879370e794c3988ec
Ivan Perminov <i.s.perminov@yandex.ru>
2021-10-06 11:19:27 +0700

    Write test for function get_line()

--- test/processfile_test.c
+++ test/processfile_test.c
@@ -9,22 +9,26 @@ CTEST(suite_processfile, get_line)
     FILE* fin = fopen("test/file_test.txt", "r");
     if (fin == NULL){
         fprintf(stderr, "Error! Can't open test/file_test.txt");
+         ASSERT_NOT_NULL(fin);
     }
     
     char* line = get_line(fin);
-    check_str("   ", line);
+    ASSERT_STR("   ", line);
 
     char* line2 = get_line(fin);
-    check_str("", line2);
+    ASSERT_STR("", line2);
 
     char* line3 = get_line(fin);
-    check_str("Math file", line3);
+    ASSERT_STR("Math file", line3);
+
+    char* line4 = get_line(fin);
+    ASSERT_STR("Math file my program word", line3);
     
     fclose(fin);
     fclose(line);
     fclose(line2);
     fclose(line3);
-    
+    fclose(line4);
     
 }
 CTEST(suite_processfile, get_lines_from_file)
