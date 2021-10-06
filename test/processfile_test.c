668f3871386da314dc8799f3ea497698bc6c3462
Ivan Perminov <i.s.perminov@yandex.ru>
2021-10-06 13:24:46 +0700

    Write test for function get_lines_from_file()

--- test/processfile_test.c
+++ test/processfile_test.c
@@ -32,4 +32,38 @@ CTEST(suite_processfile, get_line)
 }
 CTEST(suite_processfile, get_lines_from_file)
 {
+    FILE *fin = fopen("test/file_test.txt", "r");
+    if (fin == NULL) {
+        fprintf(stderr, "Error! Can't open test/file_test.txt");
+        ASSERT_NOT_NULL(fin);
+    }
+
+    
+    int N;
+    char **lines = get_lines_from_file(fin, &N);
+    
+    char* lines = get_lines_from_file(fin);
+    ASSERT_STR("   ", lines[0]);
+    ASSERT_EQUAL(11,N)
+    
+    char* lines = get_lines_from_file(fin);
+    ASSERT_STR("Math file", lines[1]);
+    ASSERT_EQUAL(11,N)
+    
+    char* lines = get_lines_from_file(fin);
+    ASSERT_STR("Math file my program word", lines[2]);
+    ASSERT_EQUAL(11,N)
+    
+
+    char* lines = get_lines_from_file(fin);
+    ASSERT_STR("1235", lines[3]);
+    ASSERT_EQUAL(11,N)
+
+        char* lines = get_lines_from_file(fin);
+    ASSERT_STR("katalog", lines[4]);
+    ASSERT_EQUAL(11,N)
+
+    char* lines = get_lines_from_file(fin);
+    ASSERT_STR("process", lines[5]);
+    ASSERT_EQUAL(11,N)
 }
